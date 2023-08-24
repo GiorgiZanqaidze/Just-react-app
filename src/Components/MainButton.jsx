3c9b1ba8a90c55fd1b48a367015eaf1d6b97d381
Giorgi Zankaidze <giorgizanqaidzeofficial@gmail.com>
2023-08-24 16:15:49 +0400

    feat: i tailwind

---
+++ src/Components/MainButton.jsx
@@ -0,0 +1,10 @@
+export default function MainButton() {
+    const gio = 'hello'
+
+
+    return (
+        <div className="text-3xl font-bold underline text-amber-950">
+            {gio}
+        </div>
+    )
+}
