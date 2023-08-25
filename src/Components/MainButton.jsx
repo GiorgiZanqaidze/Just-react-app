e1d601c909cef56230bda0c2d857c4e6950b82ae
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-08-25 13:00:39 +0400

    feat: ESlint

--- src/Components/MainButton.jsx
+++ src/Components/MainButton.jsx
@@ -1,7 +1,6 @@
 
 export default function MainButton({content, className, onClick}) {
 
-
     const buttonClassName = "rounded-xl px-14 py-2 uppercase text-white font-bold " + className
 
     return (
